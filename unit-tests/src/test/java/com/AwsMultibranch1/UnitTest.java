b9687522b04392f63128569e9e8be02c7448aa8a
mapatil1 <56800032+mapatil1@users.noreply.github.com>
2020-01-16 12:13:24 +0530

    first commit

---
+++ unit-tests/src/test/java/com/AwsMultibranch1/UnitTest.java
@@ -0,0 +1,12 @@
+package com.AwsMultibranch1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
